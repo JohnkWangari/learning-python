3af619ba315401f329e84cf2d0e63ea4eabe5885
John Karani Wangari <johnkaraniwangari@gmail.com>
2024-01-22 18:49:54 +0300

    initial commit

---
+++ datatypes.py
@@ -0,0 +1,7 @@
+name = {'a', 'b', 'c','d'}
+print(name)
+print('Hello World')
+dictionary = {'name': 'Karani',
+               'age': 22,
+               'course': 'Maths Comp Science'}
+print(dictionary)
