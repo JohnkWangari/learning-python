6d53238a37ee799754a0962e6da5a0dfc84f57c8
JohnkWangari <johnkaraniwangari@gmail.com>
2024-01-24 19:59:48 +0300

    learning variables

---
+++ variables.py
@@ -0,0 +1,38 @@
+#variables = a container for a value .
+#1 string(str)variable stores series of characters
+name = "Falcon Automation Company"
+print(type(name))
+
+
+first_name = "Falcon"
+second_name = "Automation"
+last_name = "company"
+full_name = first_name + " " +second_name + "  " +last_name
+print("The best tech company is " + full_name)
+
+
+#2  integer(int)variable stores whole numbers
+employees = 200
+print(type(employees))
+employees = employees + 50
+print(employees)
+print("company's total employees is: "+str(employees))
+
+
+#3  float variable (stores number with decimals)
+start_salary = 500.50
+print(type(start_salary))
+highest_salary = 100000
+print("start salary for interns is: "+str(start_salary)+' '+"USD")
+
+#4 boolean(bool) stores true or false value
+human = True
+machines = True
+human_machine = True
+print(type(human))
+print("both machine & human work together?:"+str(human_machine))
+
+
+
+
+
